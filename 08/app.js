eb6b64517be82b6e0e5674b58b611140a9aef0c6
JinHyeok Kang <jinhyeok.kang@gmail.com>
2015-06-04 18:16:51 +0900

    Game init

---
+++ 08/app.js
@@ -0,0 +1,94 @@
+/**
+ * Created by jhkang on 6/4/15.
+ */
+(function() {
+
+})();
+var cards = [1,2,3,4,5];
+var gameBoard = $('#game');
+var score = 0;
+var scoreBoard = $('#score');
+
+var preCard;
+
+function suffle() {
+    cards= cards.sort(function(r, l) {
+        return (Math.random() * 10) > 5;
+    });
+}
+
+function makeBoard() {
+    gameBoard.children().remove();
+    for(var i = 0; i < 2; ++i) {
+        suffle();
+        makeCardRow();
+    }
+    scoreBoard.text(score);
+}
+
+function makeCardRow() {
+    for(var i = 0; i < cards.length; ++i) {
+        var card = makeCard(cards[i]);
+        gameBoard.append(card);
+    }
+}
+
+function makeCard(num) {
+    var card = $('<div>');
+    card.attr('id', 'card');
+    card.addClass('run');
+    card.text(num);
+    return card;
+}
+
+
+function checkMatch(preCard, postCard) {
+    if(preCard == null || postCard == null) return false;
+    return preCard.text() === postCard.text();
+}
+
+function startGame() {
+    preCard = null;
+    score = 0;
+    makeBoard();
+
+    gameBoard.on('click', '.run', function(ev){
+        var card = $(ev.target);
+        if(card.hasClass('open')) return;
+        console.log(card);
+        card.addClass('open');
+        if(preCard != null) {
+            if(checkMatch(preCard, card)) {
+                preCard.removeClass('run');
+                card.removeClass('run');
+                ++score;
+                scoreBoard.text(score);
+                if(gameBoard.children('.run').length === 0) endGame();
+            } else {
+                var pre = preCard;
+                var post = card;
+                setTimeout(function() {
+                    pre.removeClass("open");
+                    post.removeClass("open");
+                }, 500);
+            }
+            preCard = null;
+        } else {
+            preCard = card;
+        }
+    });
+}
+
+function endGame() {
+    gameBoard.off('click', '**');
+    var startBtn = $('#start');
+    startBtn.show();
+    startBtn.on('click', function(ev) {
+        console.log(ev);
+        startBtn.hide();
+        startGame();
+        startBtn.off('click', "**");
+    });
+}
+
+endGame();
